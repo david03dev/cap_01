6f0607cec6e50fcb2273cda22ad86c91a9090f30
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2025-05-19 23:32:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,21 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
+driver.maximize_window()
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+sleep(5)
+
+driver.find_element(by=By.NAME,value="username").send_keys("admin")
+sleep(3)
+password = "admin123"
+driver.find_element(by=By.NAME,value="password").send_keys(password)
+sleep(3)
+login_btn = driver.find_element(by=By.XPATH,value="//button[@type='submit']")
+sleep(3)
+login_btn.click()
+sleep(5)
+
 sleep(3)
 print("browser opened successfully")
 driver.quit()
-
