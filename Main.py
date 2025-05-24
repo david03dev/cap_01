25765d6cbc227b88cfaa86268ee1c1468b4d3be6
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2025-05-24 22:39:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 #https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,22 +15,23 @@ driver.maximize_window()
 
 wait = WebDriverWait(driver,10)
 
-driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-wait.until(EC.presence_of_element_located((By.NAME,"username"))).send_keys("admin")
+def test_case_01():
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-password = "admin123"
-wait.until(EC.presence_of_element_located((By.NAME,"password"))).send_keys(password)
+    wait.until(EC.presence_of_element_located((By.NAME,"username"))).send_keys("admin")
 
-login_btn = wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='submit']")))
-login_btn.click()
+    password = "admin123"
+    wait.until(EC.presence_of_element_located((By.NAME,"password"))).send_keys(password)
 
-confirmation_status = wait.until(EC.visibility_of_element_located((By.XPATH,"//span[@class='oxd-topbar-header-breadcrumb']//h6[text()='Dashboard']"))).text
+    login_btn = wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='submit']")))
+    login_btn.click()
 
-assert "Dashboard" in confirmation_status, "TC_01 Failed"
-if "Dashboard" in confirmation_status:
-    print("TC_01 passed ")
-else:
-    print("TC_01 failed")
+    confirmation_status = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                    "//span[@class='oxd-topbar-header-breadcrumb']//h6[text()='Dashboard']"))).text
 
-driver.quit()
+    assert "Dashboard" in confirmation_status, "TC_01 Failed"
+    if "Dashboard" in confirmation_status:
+        print("TC_01 passed ")
+    else:
+        print("TC_01 failed")
